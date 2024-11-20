4b6528a2e397a22a6a2e35e70020fc1a82e7053b
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2024-11-20 10:05:14 -0500

    Update P.c

--- SHM/P.c
+++ SHM/P.c
@@ -1,67 +1,45 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#define SHMSZ 27
+#define SHM_SIZE 1024
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Uso: %s <nombre> <numero>\n", argv[0]);
-        exit(1);
+        printf("Uso: %s <string> <numero>\n", argv[0]);
+        return 1;
     }
 
-    int shmid;
-    key_t key;
-    char* shm;
-    int v = atoi(argv[2]);
+    int num = atoi(argv[2]);
+    int shm_id;
+    char *shm_ptr;
+    key_t key = ftok("shmfile", 65);
 
-    key = 5678;
-    if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) {
-        perror("Error al crear memoria compartida (shmget)");
-        exit(1);
-    }
-    if ((shm = shmat(shmid, NULL, 0)) == (char*)-1) {
-        perror("Error al adjuntar memoria compartida (shmat)");
-        exit(1);
-    }
+    shm_id = shmget(key, SHM_SIZE, 0666 | IPC_CREAT);
+    shm_ptr = (char *)shmat(shm_id, NULL, 0);
 
-    sem_t* sem_w = sem_open("/sem_w", 0);
-    sem_t* sem_r = sem_open("/sem_r", 0);
+    sem_t *sem = sem_open("/mysem", O_CREAT, 0644, 1);
+    
+    sem_wait(sem);
 
-    if (sem_w == SEM_FAILED || sem_r == SEM_FAILED) {
-        perror("Error al abrir los semáforos");
-        exit(1);
+    if (shm_ptr[2] == '3' && num == 3) {
+        snprintf(shm_ptr + 2, SHM_SIZE - 2, "%s", argv[1]);
+        printf("Memoria modificada: %s\n", shm_ptr);
+    } else {
+        printf("El nÃºmero no coincide con 3.\n");
     }
 
-    sem_wait(sem_r);
+    sem_post(sem);
 
-    char str_val[4];
-    strncpy(str_val, shm, 3);
-    str_val[3] = '\0';
-    int val_en_memoria = atoi(str_val);
+    shmdt(shm_ptr);
 
-    if (val_en_memoria == v) {
-        sem_wait(sem_w);
-
-        sprintf(shm, "%03d%s", v, argv[1]);
-        printf("Actualizado en memoria compartida: %03d%s\n", v, argv[1]);
-
-        sem_post(sem_w);
-    }
-    else {
-        printf("Valor en memoria (%03d) no coincide con el valor ingresado (%03d).\n", val_en_memoria, v);
-    }
-
-    sem_post(sem_r);
-
-    sem_close(sem_w);
-    sem_close(sem_r);
+    sem_close(sem);
+    sem_unlink("/mysem");
 
     return 0;
 }
